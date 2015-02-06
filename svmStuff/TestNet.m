fae3199e850da93ce8f9b4c8414af5e4614f2330
jarvisoa <jarvisoa@rose-hulman.edu>
2015-02-05 21:45:12 -0500

    plots roc curve

--- svmStuff/TestNet.m
+++ svmStuff/TestNet.m
@@ -8,4 +8,9 @@ realIndices = vec2ind(testT);
 testIndices = vec2ind(testY);
 
 numCorrect =sum(realIndices==testIndices);
-accuracy = numCorrect/numel(realIndices)
+accuracy = numCorrect/numel(realIndices);
+
+[c, cm] = confusion(testT,testY);
+fprintf('Percentage Correct Classification   : %f%%\n', 100*(1-c));
+fprintf('Percentage Incorrect Classification : %f%%\n', 100*c);
+plotroc(testT,testY);
