61d27247da24537dcf3dbe348c0eb6e7b8ca7a35
Mohammad Almisbaa <almisbmn@rose-hulman.edu>
2015-02-12 22:03:19 -0500

    Added a script to prepare Java input after classifying

---
+++ toJavaClass.m
@@ -0,0 +1,16 @@
+function [ javaInput ] = toJavaClass( boardImg )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+addpath('svmStuff');
+
+javaInput = zeros(9,9);
+
+for i = 1:9
+    for j = 1:9
+        square = getSquare(boardImg, i,j);
+        javaInput(i,j) = ClassifyImg(double(square));
+    end
+end
+end
+
