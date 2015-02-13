a2abb3f0de7f8b999d4069aaa6367e37412ba441
Mohammad Almisbaa <almisbmn@rose-hulman.edu>
2015-02-12 22:50:42 -0500

    Added cropSquare.m function in toJavaClass.m

--- toJavaClass.m
+++ toJavaClass.m
@@ -5,12 +5,15 @@ function [ javaInput ] = toJavaClass( boardImg )
 addpath('svmStuff');
 
 javaInput = zeros(9,9);
-
 for i = 1:9
     for j = 1:9
         square = getSquare(boardImg, i,j);
+        square = cropSquare(square);
         javaInput(i,j) = ClassifyImg(double(square));
     end
 end
+
+imshow(square);
+
 end
 
